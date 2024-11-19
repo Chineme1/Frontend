f6fad7d3a5396f3fd199852fb5ace04180ceb624
Sundara Subramanian Swaminathan <sundarasubramanian.s@gmail.com>
2024-11-19 12:59:50 -0500

    changing html fiel structure.

--- src/Components/CreateAccount/CreateAccount.jsx
+++ src/Components/CreateAccount/CreateAccount.jsx
@@ -1,36 +1,61 @@
 import React from "react";
+import { useForm, SubmitHandler } from "react-hook-form"
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 import { FaLock, FaUser } from "react-icons/fa";
 
+
 const CreateAccount = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm()
+
+  const onSubmit = (data) => console.log(data)
   return (
-    <div className="wrapper">
-      <form action="">
-        <h1>Login</h1>
-        <div className="input-box">
-          <input type="text" placeholder="Username" required />
-          <FaUser className="icon" />
-        </div>
-        <div className="input-box">
-          <input type="password" placeholder="password" required />
-          <FaLock className="icon" />
-        </div>
-        <div className="remember-forgot">
-          <label>
-            <input type="checkbox" />
-            Remember me
-          </label>
-          <a href="#">Forgot password?</a>
-        </div>
-
-        <button type="submit">Login</button>
-
-        <div className="register-link">
-          <p>
-            Don't have an account? <a href="">Register</a>
-          </p>
-        </div>
+    <div className="register-wrapper">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {/* register your input into the hook by invoking the "register" function */}
+        <Box
+          component="form"
+          sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' } }}
+          noValidate
+          autoComplete="off"
+        >
+          <div>
+            <TextField
+              required
+              id="outlined-required"
+              label="Required"
+              defaultValue="Firstname Lastname"
+            />
+            <TextField
+              id="outlined-password-input"
+              label="Password"
+              type="password"
+              autoComplete="current-password"
+            />
+            <TextField
+              id="outlined-password-input"
+              label="Password"
+              type="password"
+              autoComplete="current-password"
+            />
+            <TextField id="outlined-search" label="Search field" type="search" />
+            <TextField
+              id="outlined-helperText"
+              label="Helper text"
+              defaultValue="Default Value"
+              helperText="Some important text"
+            />
+          </div>
+        </Box>
+        <Button variant="contained">Submit</Button>
       </form>
-    </div>
+    </div >
   );
 };
 
