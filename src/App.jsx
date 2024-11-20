fcada33b3746f1b7be4b0ebb86954cbb19042f9d
Sundara Subramanian Swaminathan <sundarasubramanian.s@gmail.com>
2024-11-20 08:11:38 -0500

    fixing folder structure errors.

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import logo from './logo.svg'
+import './App.css';
+import CreateAccount from './Components/CreateAccount/CreateAccount.jsx';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Layout from './Components/Layout/temp.jsx';
+import Home from './pages/home.jsx';
+import Detail from './pages/detail.jsx';
+import LoginForm from './Components/LoginForm/LoginForm';
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/:slug" element={<Detail />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
