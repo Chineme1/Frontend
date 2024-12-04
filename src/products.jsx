4dae245e1d4716c48e23e70ba5ec72b9b0b3b874
Chineme1 <cduba@ucdavis.edu>
2024-12-03 19:33:05 -0800

    Initial commit

---
+++ src/products.jsx
@@ -0,0 +1,75 @@
+import imageProduct1 from './assets/1.png';
+import imageProduct2 from './assets/2.png';
+import imageProduct3 from './assets/3.png';
+import imageProduct4 from './assets/4.png';
+import imageProduct5 from './assets/5.png';
+import imageProduct6 from './assets/6.png';
+import imageProduct7 from './assets/7.png';
+import imageProduct8 from './assets/8.png';
+
+export const products = [
+  {
+    id: 1,
+    name: 'Atomic Habits',
+    price: 10,
+    image: imageProduct1,
+    description: 'jj',
+    slug: 'atomic-habits',
+  },
+  {
+    id: 2,
+    name: 'The Box Thief',
+    price: 90,
+    image: imageProduct2,
+    description: 'jj',
+    slug: 'the-box-thief',
+  },
+  {
+    id: 3,
+    name: 'Hell of a Book',
+    price: 60,
+    image: imageProduct3,
+    description: 'jj',
+    slug: 'hell-of-a-book',
+  },
+  {
+    id: 4,
+    name: 'Lord of the Flies',
+    price: 20,
+    image: imageProduct4,
+    description: 'jj',
+    slug: 'lord-of-the-flies',
+  },
+  {
+    id: 5,
+    name: 'The Famous Five',
+    price: 30,
+    image: imageProduct5,
+    description: 'jj',
+    slug: 'the-famous-five',
+  },
+  {
+    id: 6,
+    name: 'The War of Worlds',
+    price: 15,
+    image: imageProduct6,
+    description: 'jj',
+    slug: 'war-of-worlds',
+  },
+  {
+    id: 7,
+    name: 'World War One',
+    price: 9,
+    image: imageProduct7,
+    description: 'jj',
+    slug: 'world-war-one',
+  },
+  {
+    id: 8,
+    name: 'The War in the West',
+    price: 10,
+    image: imageProduct8,
+    description: 'jj',
+    slug: 'war-in-the-west',
+  },
+];
