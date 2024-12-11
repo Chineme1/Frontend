e6d94c5c67fd842fbaf253e466d3ab86b74086b6
Chineme1 <64276840+Chineme1@users.noreply.github.com>
2024-12-11 11:26:36 -0800

    Update layout.jsx

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import { Outlet, useLocation } from 'react-router-dom'; // Import useLocation
+import { Outlet } from 'react-router-dom';
 import Header from './header';
 import CartTab from './cartTab';
 import { useSelector } from 'react-redux';
 
 const Layout = () => {
-  const location = useLocation(); // Get the current route
   const statusTabCart = useSelector((store) => store.cart.statusTab);
-
-  const showCartTab = location.pathname !== '/checkout'; // Hide CartTab on the Checkout page
-
   return (
     <div className="bg-zinc-200">
       <main
-        className={`w-[1200px] max-w-full m-auto p-5 transform transition-transform duration-500
-        ${statusTabCart === false ? '' : '-translate-x-56'}`}
+        className={w-[1200px] max-w-full m-auto p-5 transform transition-transform duration-500
+        ${statusTabCart === false ? '' : '-translate-x-56'}}
       >
         <Header />
         <Outlet />
       </main>
-      {showCartTab && <CartTab />} {/* Conditionally render CartTab */}
+      <CartTab />
     </div>
   );
 };
