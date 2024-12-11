f5711b26aa2aabd1d443e3c8e8a208824ac81bb4
Chineme1 <64276840+Chineme1@users.noreply.github.com>
2024-12-10 19:41:37 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import Layout from './components/layout';
 import Home from './pages/home';
 import Detail from './pages/detail';
+import Checkout from './pages/Checkout'; // Import Checkout page
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="/:slug" element={<Detail />} />
+          <Route path="/checkout" element={<Checkout cartItems={cartItems} />} />
         </Route>
       </Routes>
     </BrowserRouter>
